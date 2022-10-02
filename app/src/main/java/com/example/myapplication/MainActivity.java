f79d051136ea789c9e3b282120c67a295aab1cdb
Divesh Ahuja <divesh@deepspace9.tech>
2022-10-02 10:55:20 +0530

    Tested bottom nav without adapter

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,48 +1,46 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView userListRecyclerView;
-    private ArrayList<User> userArrayList = new ArrayList<>();
-    private UserAdapter userAdapter;
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        setData();
+        initViews();
+        initListeners();
     }
 
-    private void initView(){
-        userListRecyclerView = findViewById(R.id.userListRecyclerview);
-        userListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    private void initViews(){
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 
-    private void setData(){
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", false));
-        userArrayList.add(new User("Mohit Singh", false));
-        userArrayList.add(new User("Johnny Baba", true));
-        userArrayList.add(new User("Burj Khalifa", false));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-
-        userAdapter = new UserAdapter(this, userArrayList);
-        userListRecyclerView.setAdapter(userAdapter);
+    private void initListeners(){
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.page_1){
+                Toast.makeText(MainActivity.this, "Pag1", Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(MainActivity.this, "Pag2", Toast.LENGTH_LONG).show();
+            }
+            return false;
+        });
     }
 }
