a0cfd8b0e8d2e67cef1a297d7ef0b74cf9f1b316
Divesh Ahuja <divesh@deepspace9.tech>
2022-10-09 17:36:54 +0530

    Updated Main Activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] listOfTitles = new String[]{"Favorites", "Playlist", "Recent"};
-    private ViewPager2 viewPager;
-    private TabLayout tabLayout;
-
+    private EditText nameEdt, jobEdt;
+    private Button postDataBtn;
+    private ProgressBar loadingPB;
+    private TextView responseTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,20 +27,49 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         initListeners();
     }
-
-    private void initViews() {
-        viewPager = findViewById(R.id.view_pager);
-        tabLayout = findViewById(R.id.tab_layout);
+    private void initViews(){
+        nameEdt = findViewById(R.id.idEdtName);
+        jobEdt = findViewById(R.id.idEdtJob);
+        postDataBtn = findViewById(R.id.idBtnPost);
+        loadingPB = findViewById(R.id.idLoadingPB);
+        responseTV = findViewById(R.id.idTVResponse);
     }
 
-    private void initListeners() {
-        MainViewPagerAdapter pagerAdapter = new MainViewPagerAdapter(this);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setCurrentItem(0);
-        addTabLayoutMediator();
+    private void initListeners(){
+        postDataBtn.setOnClickListener(view -> {
+            if(nameEdt.getText().toString().isEmpty() || jobEdt.getText().toString().isEmpty()){
+                Toast.makeText(this, "Invalid Form", Toast.LENGTH_SHORT).show();
+            }else{
+                postData(nameEdt.getText().toString(), jobEdt.getText().toString());
+            }
+        });
     }
 
-    private void addTabLayoutMediator() {
-        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText("Tab: " + position)).attach();
+    private void postData(String name, String job) {
+        loadingPB.setVisibility(View.VISIBLE);
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://reqres.in/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        RetrofitApiInterface retrofitApiInterface = retrofit.create(RetrofitApiInterface.class);
+        FormRequest formRequest = new FormRequest(name, job);
+        Call<FormRequest> call = retrofitApiInterface.registerUser(formRequest);
+        call.enqueue(new Callback<FormRequest>() {
+            @Override
+            public void onResponse(Call<FormRequest> call, Response<FormRequest> response) {
+                Toast.makeText(MainActivity.this, "Data added to API", Toast.LENGTH_SHORT).show();
+                loadingPB.setVisibility(View.GONE);
+                jobEdt.setText("");
+                nameEdt.setText("");
+                FormRequest responseFromAPI = response.body();
+                String responseString = "Response Code : " + response.code() + "\nName : " + responseFromAPI.getName() + "\n" + "Job : " + responseFromAPI.getJob();
+                responseTV.setText(responseString);
+            }
+
+            @Override
+            public void onFailure(Call<FormRequest> call, Throwable t) {
+                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Network Error occurred", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
