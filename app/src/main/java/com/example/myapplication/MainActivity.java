e963c6b0b54d954d019cc5914afe2c8e7b0cbabc
Divesh Ahuja <divesh@deepspace9.tech>
2022-10-31 21:47:48 +0530

    Success

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,51 +1,25 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements UserViewInterface {
-    private RecyclerView userListRecyclerView;
-    private ArrayList<User> userArrayList = new ArrayList<>();
-    private UserAdapter userAdapter;
+public class MainActivity extends AppCompatActivity{
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            Toast.makeText(this, "Thanks for giving permissions.", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permission disallowed.", Toast.LENGTH_SHORT).show();
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        setData();
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
     }
-
-    private void initView(){
-        userListRecyclerView = findViewById(R.id.userListRecyclerview);
-        userListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-    }
-
-    private void setData(){
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", false));
-        userArrayList.add(new User("Mohit Singh", false));
-        userArrayList.add(new User("Johnny Baba", true));
-        userArrayList.add(new User("Burj Khalifa", false));
-        userArrayList.add(new User("Divesh Ahuja", true));
-        userArrayList.add(new User("Divesh Ahuja", true));
-
-        userAdapter = new UserAdapter(this, userArrayList, this);
-        userListRecyclerView.setAdapter(userAdapter);
-    }
-
-    @Override
-    public void onNameClicked(String name) {
-        Toast.makeText(this, "Now in activity: " + name, Toast.LENGTH_SHORT).show();
-    }
-
-
-
 }
