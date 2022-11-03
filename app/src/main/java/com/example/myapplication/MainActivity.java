c9692698e96f30d2ac2d789b185f2a114c02ece4
Divesh Ahuja <divesh@deepspace9.tech>
2022-11-03 08:20:27 +0530

    Tested

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,23 +3,66 @@ package com.example.myapplication;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
-import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.provider.MediaStore;
+import android.widget.ImageView;
+import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity{
-    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-        if (isGranted) {
-            Toast.makeText(this, "Thanks for giving permissions.", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Permission disallowed.", Toast.LENGTH_SHORT).show();
-        }
-    });
+public class MainActivity extends AppCompatActivity {
+    private AppCompatButton chooseImageBtn;
+    private ImageView imageView;
+    private final ActivityResultLauncher<Intent> imageChooserLauncher
+            = registerForActivityResult(
+            new ActivityResultContracts
+                    .StartActivityForResult(),
+            result -> {
+                if (result.getResultCode()
+                        == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    // do your operation from here....
+                    if (data != null
+                            && data.getData() != null) {
+                        Uri selectedImageUri = data.getData();
+                        Bitmap selectedImageBitmap;
+                        try {
+                            selectedImageBitmap
+                                    = MediaStore.Images.Media.getBitmap(
+                                    this.getContentResolver(),
+                                    selectedImageUri);
+                            imageView.setImageBitmap(selectedImageBitmap);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        initViews();
+        initListeners();
+    }
+
+    private void initListeners() {
+        chooseImageBtn.setOnClickListener(view -> {
+            Intent imageChooserIntent = new Intent();
+            imageChooserIntent.setType("image/*");
+            imageChooserIntent.setAction(Intent.ACTION_GET_CONTENT);
+            imageChooserLauncher.launch(imageChooserIntent);
+        });
+    }
+
+    private void initViews() {
+        imageView = findViewById(R.id.imageView);
+        chooseImageBtn = findViewById(R.id.chooseImageBtn);
     }
 }
